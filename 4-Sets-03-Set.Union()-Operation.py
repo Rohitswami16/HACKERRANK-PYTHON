d86127aa0aa5c4c3d0d2cb84d0b8e3ac232a7c0a
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-12-05 17:49:29 +0530

    Create 4-Sets-03-Set.Union()-Operation.py

---
+++ 4-Sets-03-Set.Union()-Operation.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+set1 = set(map(int, input().split(" ")))
+
+n2 = int(input())
+
+set2 = set(map(int, input().split()))
+
+set3 = set(set1.union(set2))
+
+print(len(set3))
