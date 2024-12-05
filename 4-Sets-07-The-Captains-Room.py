7b696c4db1db9c4e0068a98cd4e1c89817415cc8
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-12-05 17:58:28 +0530

    Create 4-Sets-07-The-Captains-Room.py

---
+++ 4-Sets-07-The-Captains-Room.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+rooms = list(map(int, input().split()))
+
+room = set(rooms)
+
+c = (sum(room)*n-sum(rooms))//(n-1)
+print(c)
+
